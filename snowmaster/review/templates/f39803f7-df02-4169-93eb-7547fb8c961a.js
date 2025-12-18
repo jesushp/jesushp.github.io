a2e5171817927e30419ea0d08692168da6ec8c97
jesushp <gsushp@gmail.com>
2025-12-19 00:08:19 +0100

    Add files via upload

---
+++ snowmaster/review/templates/f39803f7-df02-4169-93eb-7547fb8c961a.js
@@ -0,0 +1,14 @@
+var content='<div class="ui-page " deviceName="iphone11pro" deviceType="mobile" deviceWidth="375" deviceHeight="812">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devMobile devIOS iphone-device canvas firer commentable non-processed" alignment="left" name="Template 1"width="375" height="812">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./review/templates/f39803f7-df02-4169-93eb-7547fb8c961a/style-1720520777139.css" />\
+      <div class="freeLayout">\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
